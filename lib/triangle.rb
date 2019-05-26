70c5374b66f1ed76e12a9676d589ed5fd1c31eb0
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 21:54:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,13 @@ class Triangle
     raise TriangleError 
     
   else 
-    if :one == :two && :one == :three
+    if :one == :two && :one == :three 
+      :equilateral 
+      
+      elsif (:one == :two) && (:one != :three) && (:two != :three) || (:two == :three) && (:two != :one) && (:three != :one) || (:one == :three)
+      :isoceles 
+      
+    else (:one != :two) || (:two != :three) || (:three != :one)
     
     
   end
