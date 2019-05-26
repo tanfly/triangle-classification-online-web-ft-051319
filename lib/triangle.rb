beaa695bda6a344968b932305c92e30545463e4b
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 21:40:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,4 +3,8 @@ class Triangle
   attr_accessor :equilateral, :isoceles, :scalene 
   
   def initialize(equilateral, isoceles, scalene)
+    @equilateral = equilateral
+    @isoceles = isoceles 
+    @scalene = scalene
+  end 
 end
